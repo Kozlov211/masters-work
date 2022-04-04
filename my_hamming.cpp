f09ba9faf9598a09016a26e439f4970cc80295a8
kozlov211 <sayf1ck64.7@gmail.com>
2022-04-04 20:58:18 +0300

    develompent my_hamming

---
+++ my_hamming.cpp
@@ -0,0 +1,139 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <map>
+#include <cmath>
+
+template <typename Type>
+void PrintVector(const std::vector<Type>& S) {
+	// Печать вектора в консоль
+	for (const Type& num : S) {
+		std::cout << num << std::endl;
+	}
+}
+
+template <typename Type> 
+void Print2dVector(const std::vector<std::vector<Type>>& vec) {
+	for (const std::vector<Type>& nums : vec) {
+		for (const Type& num : nums) {
+			std::cout << num << "\t";
+		}
+		std::cout << std::endl;
+	} 
+}
+
+template <typename Type>
+void VectorTo2dVector(std::vector<std::vector<Type>>& vec2d, const std::vector<Type>& vec) {
+		for (size_t i = 0; i < vec2d.size(); ++i) {
+				for (size_t j = 0; j < vec2d[0].size(); ++j) {
+						vec2d[i][j] = vec[i * vec2d[0].size() + j];
+				}
+		}
+
+}
+
+std::vector <uint32_t> GenerateNumbers(const uint32_t& alphabet, const uint32_t& alphabet_length, std::vector<uint32_t> prefix, std::vector<uint32_t> arrays) { 
+		if (alphabet_length == 0) { 
+				for (const uint32_t& n : prefix) {
+						arrays.push_back(n); 
+				} 
+        return arrays; 
+
+    } 
+    for (int digit = 0; digit < alphabet; digit++) { 
+        prefix.push_back(digit); 
+		arrays = GenerateNumbers(alphabet, alphabet_length - 1, prefix, arrays); 
+	   	prefix.pop_back(); 
+  
+    } 
+     return arrays; 
+}
+
+void RandBits(std::vector<uint32_t>& bits) {
+	// Генерация битовой последовательности
+	std::random_device rnd;
+	std::mt19937 gen(rnd());
+	std::uniform_int_distribution<uint32_t> number(0, 1);
+	for (uint32_t& bit : bits) {
+		bit = number(gen);
+	}
+}
+
+void AddNormalNoise(std::vector<double>& signal,const double& mean, const double& dispersion) {
+	// АБГШ к сигналу
+	std::mt19937 generator(rand());
+	double standart_deviation = sqrt(dispersion);
+	std::normal_distribution<double> distribution(mean,standart_deviation);
+	for (double& sample : signal) {
+			sample += distribution(generator);
+	}
+}
+
+void HammingCode(const std::vector<std::vector<uint32_t>>& inf_bits, std::vector<std::vector<uint32_t>>& code_sequence) {
+	for (size_t i = 0; i < inf_bits.size(); ++i) {
+			std::copy(inf_bits[i].begin(), inf_bits[i].end(), code_sequence[i].begin());
+			code_sequence[i][4] = (inf_bits[i][0] + inf_bits[i][1] + inf_bits[i][2]) % 2;
+			code_sequence[i][5] = (inf_bits[i][1] + inf_bits[i][2] + inf_bits[i][3]) % 2;
+			code_sequence[i][6] = (inf_bits[i][0] + inf_bits[i][1] + inf_bits[i][3]) % 2;
+	}
+}
+
+void ProbabilityOfReceivedBits(std::vector<double>& probability_of_received_bits) {
+	std::random_device rnd;
+	std::mt19937 gen(rnd());
+	std::uniform_real_distribution<double> result(0, 1);
+	for (double& element : probability_of_received_bits) {
+			element = result(gen);
+	}
+} 
+
+double SequenceProbability(const std::vector<uint32_t>& code_sequence, const std::vector<double>& probability_of_received_bits, const std::vector<uint32_t> state) {
+		
+
+} 
+
+void HammingDecode(const std::vector<std::vector<uint32_t>>& code_sequence, std::vector<std::vector<uint32_t>>& inf_bits) {
+	std::vector<uint32_t> prefix;
+	std::vector<uint32_t> tmp;
+	tmp = GenerateNumbers(2, code_sequence[0].size(), prefix, tmp);
+	std::vector<std::vector<uint32_t>> all_states (pow(2, code_sequence[0].size()), std::vector<uint32_t> (code_sequence[0].size()));
+	VectorTo2dVector(all_states, tmp);
+	std::vector<double> probability_of_received_bits (code_sequence[0].size());
+	std::vector<double> sequence_probability (pow(2, code_sequence[0].size()));
+	ProbabilityOfReceivedBits(probability_of_received_bits);
+		
+	
+}
+
+uint32_t ModTwoAddVectors(const std::vector<uint32_t>& vec1, const std::vector<uint32_t> vec2) {
+	uint32_t result = 0;
+	for (size_t i = 0; i < vec1.size(); ++i) {
+			result += (vec1[i] + vec2[i]) % 2;
+	}
+	return result;
+}
+
+uint32_t CheckError(const std::vector<std::vector<uint32_t>>& out_bits, const std::vector<std::vector<uint32_t>>& in_bits) {
+	uint32_t errs = 0;
+	for (size_t i = 0; i < out_bits.size(); ++i) {
+			errs += ModTwoAddVectors(out_bits[i], in_bits[i]);
+	}
+	return errs;
+}
+
+
+
+
+int main () {
+	uint32_t block = 10;
+	std::vector<std::vector<uint32_t>> inf_bits (block, std::vector<uint32_t> (4)); // Информационные биты (кратны 4)
+	for (std::vector<uint32_t>& bits : inf_bits) {
+		RandBits(bits);
+	}
+	std::vector<std::vector<uint32_t>> code_sequence (block, std::vector<uint32_t> (7)); // Код Хэмминга 7,4,3
+	HammingCode(inf_bits, code_sequence);
+	std::vector<std::vector<uint32_t>> bits (block, std::vector<uint32_t> (4)); // Информационные биты (кратны 4)
+	HammingDecode(code_sequence, bits);
+	return 0;
+}
