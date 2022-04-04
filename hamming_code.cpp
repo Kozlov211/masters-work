859059bdb9548b6bfd6216c2ee65fe21c13c9f5e
kozlov211 <sayf1ck64.7@gmail.com>
2022-04-04 15:11:14 +0300

    HammingCode

---
+++ hamming_code.cpp
@@ -0,0 +1,66 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <random>
+
+template <typename Type>
+void PrintVector(const std::vector<Type>& S) {
+	// Печать вектора в консоль
+	for (const Type& num : S) {
+		std::cout << num << std::endl;
+	}
+}
+
+template <typename Type> 
+void Print2dVector(const std::vector<std::vector<Type>>& vec) {
+	for (const std::vector<Type>& nums : vec) {
+		for (const Type& num : nums) {
+			std::cout << num << "\t";
+		}
+		std::cout << std::endl;
+	} 
+}
+
+void RandBits(std::vector<uint32_t>& bits) {
+	// Генерация битовой последовательности
+	std::random_device rnd;
+	std::mt19937 gen(rnd());
+	std::uniform_int_distribution<uint32_t> number(0, 1);
+	for (uint32_t& bit : bits) {
+		bit = number(gen);
+	}
+}
+
+void AddNormalNoise(std::vector<double>& signal,const double& mean, const double& dispersion) {
+	// АБГШ к сигналу
+	std::mt19937 generator(rand());
+	double standart_deviation = sqrt(dispersion);
+	std::normal_distribution<double> distribution(mean,standart_deviation);
+	for (double& sample : signal) {
+			sample += distribution(generator);
+	}
+}
+
+void HammingCode(const std::vector<std::vector<uint32_t>>& inf_bits, std::vector<std::vector<uint32_t>>& code_sequence) {
+	for (size_t i = 0; i < inf_bits.size(); ++i) {
+			std::copy(inf_bits[i].begin(), inf_bits[i].end(), code_sequence[i].begin());
+			code_sequence[i][4] = (inf_bits[i][0] + inf_bits[i][1] + inf_bits[i][2]) % 2;
+			code_sequence[i][5] = (inf_bits[i][1] + inf_bits[i][2] + inf_bits[i][3]) % 2;
+			code_sequence[i][6] = (inf_bits[i][0] + inf_bits[i][1] + inf_bits[i][3]) % 2;
+	}
+}
+
+int main () {
+	uint32_t block = 10;
+	std::vector<std::vector<uint32_t>> inf_bits (block, std::vector<uint32_t> (4)); // Информационные биты (кратны 4)
+	for (std::vector<uint32_t>& bits : inf_bits) {
+		RandBits(bits);
+	}
+	Print2dVector(inf_bits);
+	std::vector<std::vector<uint32_t>> code_sequence (block, std::vector<uint32_t> (7)); // Код Хэмминга 7,4,3
+	std::cout <<'\n';
+	HammingCode(inf_bits, code_sequence);
+	Print2dVector(code_sequence);	
+		
+	return 0;
+}
